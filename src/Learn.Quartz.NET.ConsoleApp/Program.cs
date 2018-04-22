ee94143bc5779026dc84ee2d862f48f98ef65510
陈亮 <taohonggou@outlook.com>
2018-04-22 19:59:10 +0800

    添加了控制台程序

---
+++ src/Learn.Quartz.NET.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learn.Quartz.NET.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
